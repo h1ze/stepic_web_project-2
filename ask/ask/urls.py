43c94a36655525b42430ad675087b816c6cd2c8f
h1ze <burgov@mail.ru>
2016-03-14 18:55:49 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -9,10 +9,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'qa.views.home', name='home'),
+    url(r'^$', 'qa.views.test'),
     url(r'^login/', 'qa.views.test'),
     url(r'^signup', 'qa.views.test'),
-    url(r'^question/(?P<id>\d+)', 'qa.views.question', name='question'),
+    url(r'^question/(?P<id>\d+)', 'qa.views.test'),
     url(r'^ask/', 'qa.views.test'),
     url(r'^popular/', 'qa.views.test'),
     url(r'^new', 'qa.views.test')
