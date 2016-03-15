873503ed840d51dbfc18466e63346f7fd14fe855
h1ze <burgov@mail.ru>
2016-03-15 17:00:05 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,8 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
+sudo pip istall django-autofixture
+sudo /etc/init.d/mysql start
+
 #gunicorn -b 0.0.0.0:8080 hello:app
 #./script.sh
